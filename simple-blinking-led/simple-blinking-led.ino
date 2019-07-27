c73ea2e3cc919e3a34028523c243d47a4fe846ba
Adam Corrie <apc@adamcorrie.com>
2019-07-27 15:36:43 +0100

    Create simple-blinking-led.ino

---
+++ simple-blinking-led/simple-blinking-led.ino
@@ -0,0 +1,23 @@
+/*
+ * Blinking LED
+ * 
+ * Connect an LED and a suitable current-limiting resistor
+ * between Pin 13 and GND. Or use the built-in LED. Up to you.
+ * 
+ * The LED will blink on and off.
+ * 
+ * Created by Adam Corrie on 27/07/2019
+ * Licensed under the GNU General Public License v3.0
+ * 
+ */
+
+void setup() {
+  pinMode(13, OUTPUT); // configure Pin 13 as an OUTPUT pin
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // set Pin 13 to HIGH (5V)
+  delay(1000); // wait 1000ms
+  digitalWrite(13, LOW); // set Pin 13 to LOW (0V)
+  delay(1000); // wait 1000ms
+}
